c792c349b7c5e92cee52b1b6eb8943961a9122a4
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-05-18 23:36:51 -0400

    stuck at factory method

--- lib/03_temperature_object.rb
+++ lib/03_temperature_object.rb
@@ -1,3 +1,52 @@
 class Temperature
-  # TODO: your code goes here!
+  # your code goes here!
+  def initialize(hash)
+    @degree = hash.values[0]
+    @unit = hash.keys[0]
+  end
+
+  def self.from_celsius
+  end
+
+  def self.from_fahrenheit
+  end
+
+  def in_fahrenheit
+    return @degree * 9.0 / 5.0 + 32 if @unit == :c
+    @degree
+  end
+
+  def in_celsius
+    return (@degree - 32) * 5 / 9 if @unit == :f
+    @degree
+  end
 end
+
+class Celsius < Temperature
+  def initialize(degree)
+    @degree = degree
+  end
+
+  def in_fahrenheit
+    return @degree * 9.0 / 5.0 + 32
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(degree)
+    @degree = degree
+  end
+
+  def in_celsius
+    return (@degree - 32) * 5 / 9
+  end
+
+end
+  # def ftoc(fahrenheit)
+  #   (fahrenheit - 32) * 5/9
+  # end
+  #
+  # def ctof(celsius)
+  #   #debugger
+  #   celsius * 9.0 / 5.0 + 32
+  # end
