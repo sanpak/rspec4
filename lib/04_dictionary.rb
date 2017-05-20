bce85034a2a9f134fc1335f00192e5fd39bfcc8d
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-05-19 20:53:22 -0400

    took so long, but solved 04

--- lib/04_dictionary.rb
+++ lib/04_dictionary.rb
@@ -1,3 +1,39 @@
 class Dictionary
-  # TODO: your code goes here!
+  #  your code goes here!
+  def initialize
+    @entry = {}
+  end
+
+  def entries
+    @entry
+  end
+
+  def add(hash)
+    if hash.class == Hash
+      @entry[hash.keys[0]] = hash.values[0]
+    else
+      @entry[hash] = nil
+    end
+  end
+
+  def include?(key)
+    @entry.include?(key)
+  end
+
+  def find(key)
+    return @entry.select { |k,v| k.include?(key) }
+  end
+
+  def keywords
+    @entry.keys.sort
+  end
+
+  def printable
+    result = ""
+    keywords.each do |key|
+      result += "[#{key}] \"#{@entry[key]}\"\n"
+    end
+    result.chomp
+  end
+
 end
